651f7da1540280ff46085a9bd89ca1415414ce41
samuel <samuel@mail.com>
2023-04-11 15:16:00 -0300

    js funcionando

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-const linhaCep = document.querySelector('[data-js-info = "cep"]')
-const linhaLocalidade = document.querySelector('[data-js-info = "localidade"]')
-const linhaLogradouro = document.querySelector('[data-js-info = "logradouro"]')
-const linhaIbge= document.querySelector('[data-js-info = "ibge"]')
+const linhaCep = document.querySelector('[data-js-info = "cep"]');
+const linhaLocalidade = document.querySelector('[data-js-info = "localidade"]');
+const linhaUf = document.querySelector('[data-js-info = "uf"]');
+const linhaDdd= document.querySelector('[data-js-info = "ddd"]');
 
-const txtCep = document.querySelector('[data-js-txtCep]')
+const txtCep = document.querySelector('[data-js-txtCep]');
 const btnBuscar = document.querySelector('[data-js-btnBuscar]');
 
 function buscarDados(){
     const cepDigitado = txtCep.value;
     fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`)
-        .then(response => response.json())
-        .then(cep =>{
-            linhaCep.innerText = cep.cep;
-            linhaLocalidade.innerText = (cep.localidade || 'Informação indisponível');
-            linhaLogradouro.innerText = cep.logradouro;
-            linhaIbge.innerText = cep.ibge;
-        }
-    );
+    .then(response => response.json())
+    .then(cep =>{
+        console.log(cep);
+        linhaCep.innerText = cep.cep;
+        linhaLocalidade.innerText = cep.localidade;
+        linhaUf.innerText = cep.uf;
+        linhaDdd.innerText = cep.ddd;
+    })
 }
 
 btnBuscar.addEventListener('click', buscarDados);
